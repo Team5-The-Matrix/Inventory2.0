618ec34479f154a3656ee219b6f42dcf9b4a3b6b
tluke3 <tluke3@msudenver.edu>
2022-04-17 19:12:27 -0600

    ProfitBetweenDates class/fixed OverallProfit bugs

--- Admin/ProfitCalculation.java
+++ Admin/ProfitCalculation.java
@@ -3,7 +3,11 @@
 
 package Admin;
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 
 public class ProfitCalculation{
     static Connection conn = null;
@@ -16,12 +20,13 @@ public class ProfitCalculation{
         double totalCost = 0;
         try{
             PreparedStatement profitStmt = conn.prepareStatement
-            ("SELECT quantity, wholesale_cost, sale_price FROM customer_orders");//change customer_orders if needed
+            ("SELECT product_id FROM customer_orders");//change customer_orders if needed
             ResultSet profitRS = profitStmt.executeQuery();
             while(profitRS.next()){
-                qty = Double.parseDouble(profitRS.getString("quantity"));
-                wholesaleCost = Double.parseDouble(profitRS.getString("wholesale_cost"));
-                salePrice = Double.parseDouble(profitRS.getString("sale_price"));
+                String[] str = Search(profitRS.getString("product_id"));
+                qty = Double.parseDouble(str[1]);
+                wholesaleCost = Double.parseDouble(str[2]);
+                salePrice = Double.parseDouble(str[3]);
                 totalGain = totalGain + (qty * salePrice);
                 totalCost = totalCost + (qty * wholesaleCost);
             }
@@ -41,6 +46,41 @@ public class ProfitCalculation{
         try{
             for(int i = 0; i < itemIDs.size(); i++){
                 String[] str = Search(itemIDs.get(i));
+                qty = Double.parseDouble(str[1]);
+                wholesaleCost = Double.parseDouble(str[2]);
+                salePrice = Double.parseDouble(str[3]);
+                totalGain = totalGain + (qty * salePrice);
+                totalCost = totalCost + (qty * wholesaleCost);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return totalGain - totalCost;
+    }
+
+
+    //Calculate profit between date1 (less recent) and date2 (more recent)
+    public static double ProfitBetweenDates(String date1, String date2) throws ParseException{ //dates should be passed as "MM/DD/YYYY"
+        double qty, wholesaleCost, salePrice;
+        double totalGain = 0;
+        double totalCost = 0;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        Date d1 = dateFormat.parse(date1);
+        Date d2 = dateFormat.parse(date2);
+        ArrayList<String> productIDs = new ArrayList<>();
+        Iterator<String> itr = productIDs.iterator();
+        try{
+            PreparedStatement profitStmt = conn.prepareStatement
+            ("SELECT product_id, date FROM customer_orders");//change customer_orders if needed
+            ResultSet profitRS = profitStmt.executeQuery();
+            while(profitRS.next()){
+                if(d1.before(d2)){
+                    productIDs.add(profitRS.getString("product_id"));
+                }
+            }
+            while(itr.hasNext()){
+                String[] str = Search(itr.next());
                 qty = Double.parseDouble(str[1]);
                 wholesaleCost = Double.parseDouble(str[2]);
                 salePrice = Double.parseDouble(str[3]);
