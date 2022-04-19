ccf14ccd7b0cad0b85dd8faeb9ff68d5a8ee4449
noahpk2 <noah@pearsonkramer.com>
2022-04-19 07:31:09 -0600

    deleted login.java because bugs were preventing build

--- Admin/ReportGUI.java
+++ Admin/ReportGUI.java
@@ -2,16 +2,38 @@
 // Author(s): Noah Pearson Kramer
 // GUI For Database Analysis tools - Charts, Simulation, etc
 
+
+
+
 package Admin;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.JTableHeader;
+import java.awt.Color;
 
 public class ReportGUI {
-    
+
+public static void main(String[] args){
+
+    JFrame page = new JFrame("Report Tools");
+    JButton SimulateOrders = new JButton("Simulate Orders");
+    JButton OrderHistory = new JButton("Order History");
+    JButton FinancialProjection = new JButton("Financial Projections");
+    JButton ProfitLoss = new JButton("Profit/ Loss Calculator");
+    JPanel panel = new JPanel();
+
+    panel.setBounds(400, 200,200,200);
+    SimulateOrders.setBounds(20,20,200,30);
+    OrderHistory.setBounds(20,60,200,30);
+    FinancialProjection.setBounds(20,100, 200,30);
+    ProfitLoss.setBounds(20,140,200,30);
+
+    page.add(SimulateOrders);
+    page.add(OrderHistory);
+    page.add(FinancialProjection);
+    page.add(ProfitLoss);
+    panel.setBackground(Color.green);
+    page.add(panel);
+    page.setSize(1200,800);
+    page.setVisible(true);
+    page.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
--- Admin/ReportTools.java
+++ Admin/ReportTools.java
@@ -5,6 +5,8 @@
 
 package Admin;
 
+import java.util.Scanner;
+
 public class ReportTools {
 
     public static void main(String[] args){}
@@ -14,21 +16,27 @@ public class ReportTools {
     // TAKES CSV OF SAMPLE ORDERS AND RUNS THEM ON DATABASE 
     public void SimulateOrders(){
 
+        Scanner sc = new Scanner(System.in);
+        //TODO : READ CSV, RUN SIMULATED ORDERS ON DATABASE, CREATE NEW TABLE TO HOLD ORDER HISTORY
+
     }
 
     // Pulls data from database and order history for charting
     public void ChartData (){
 
+        //TODO: CALL ORDER HISTORY FROM DATABASE, PASS TO GUI
 
     }
     //Creates projection based on linear regression of sales
     public void FinancialProjection(){
 
-
+        //TAKE PAST ORDER HISTORY, DO A LINEAR REGRESSION FIT TO PROJECT SALES
     }
 
     //calls profitcalculation.java, etc
     public void ProfitLoss(){
+
+        // CALL IN JUNES PROFIT CALCULATOR
 
     }
     
